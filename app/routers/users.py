abee33959c2da7969b08d4a1d859ed6dd5d9e7fb
ifsharap <ifsharap@mts.ru>
2025-02-01 11:05:58 +0300

    added tests

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 from typing import Iterable
 
 from fastapi import APIRouter, HTTPException, Depends
+from pydantic import ValidationError
 from sqlmodel import Session
 from app.models.User import User, UserCreate, UserUpdate
 from app.database.engine import get_session
@@ -26,13 +27,20 @@ def get_users(session: Session = Depends(get_session)) -> Iterable[User]:
 
 @router.post("/", status_code=HTTPStatus.CREATED)
 def create_user(user: User, session: Session = Depends(get_session)) -> User:
-    UserCreate.model_validate(user.model_dump())
+    try:
+        UserCreate.model_validate(user.model_dump())
+    except ValidationError:
+        raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY)
     return users.create_user(session, user)
 
 @router.patch("/{user_id}", status_code=HTTPStatus.OK)
 def update_user(user_id: int, user: User, session: Session = Depends(get_session)) -> User:
     if user_id < 1:
         raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail="Invalid user id")
+
+    if not users.get_user(session, user_id):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
+
     UserUpdate.model_validate(user.model_dump())
     return users.update_user(session, user_id, user)
 
@@ -40,7 +48,13 @@ def update_user(user_id: int, user: User, session: Session = Depends(get_session
 def delete_user(user_id: int, session: Session = Depends(get_session)):
     if user_id < 1:
         raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail="Invalid user id")
+
+    user = users.get_user(session, user_id)
+    if not user:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
+
     users.delete_user(session, user_id)
     return {"message": "User deleted"}
+
 
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,8 @@ import pytest
 import requests
 from app.models.User import User
 
+DELETED_MESSAGE = {"message": "User deleted"}
+USER_NOT_FOUND = {"detail": "User not found"}
 
 @pytest.fixture(scope="module")
 def fill_test_data(app_url):
@@ -24,6 +26,15 @@ def fill_test_data(app_url):
 
 @pytest.fixture()
 def create_user(app_url):
+    user_data = dict(email="vasya.pupov@reqres.in", first_name="Vasya", last_name="Pupov",
+                         avatar="https://reqres.in/img/faces/10-image.jpg")
+    response = requests.post(f"{app_url}/api/users/", json=user_data)
+    assert response.status_code == HTTPStatus.CREATED
+    created_user_id = response.json()["id"]
+    yield created_user_id
+
+@pytest.fixture()
+def create_user_and_delete(app_url):
     user_data = dict(email="tobias.funke@reqres.in", first_name="Ilnur", last_name="Sharapov",
                          avatar="https://reqres.in/img/faces/9-image.jpg")
     response = requests.post(f"{app_url}/api/users/", json=user_data)
@@ -41,6 +52,7 @@ def users(app_url):
     return response.json()
 
 class TestApi:
+
     @pytest.mark.usefixtures("fill_test_data")
     def test_users(self, app_url):
         response = requests.get(f"{app_url}/api/users/")
@@ -76,18 +88,43 @@ class TestApi:
         response = requests.get(f"{app_url}/api/users/{user_id}")
         assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
-    def test_get_after_create_and_update(self, app_url, create_user):
-        # user_data = dict(email="tobias.funke@reqres.in", first_name="Tobias", last_name="Funke",
-        #                  avatar="https://reqres.in/img/faces/9-image.jpg")
-        #
-        # create_user_response = requests.post(f"{app_url}/api/users/", json=user_data)
-        #
-        # assert create_user_response.status_code == HTTPStatus.CREATED
-        #
-        # user_id = create_user_response.json()["id"]
+    @pytest.mark.parametrize("user_data", [dict(email="test1.test1@reqres.in", first_name="Test1", last_name="Test1",
+                         avatar="https://reqres.in/img/faces/9-image.jpg")])
+    def test_create_user(self, app_url, user_data):
+        created_user_response = requests.post(f"{app_url}/api/users/", json=user_data)
+        assert created_user_response.status_code == HTTPStatus.CREATED
+        created_user_data = created_user_response.json()
+        assert created_user_data["email"] == user_data["email"]
+        assert created_user_data["first_name"] == user_data["first_name"]
+        assert created_user_data["last_name"] == user_data["last_name"]
+        assert created_user_data["avatar"] == user_data["avatar"]
 
-        user_data = create_user
+    def test_update_user(self, app_url, create_user):
+        user_id = create_user
+        update_data = dict(first_name="PATCH1", last_name="METHOD")
 
+        update_data_response = requests.patch(f"{app_url}/api/users/{user_id}", json=update_data)
+        assert update_data_response.status_code == HTTPStatus.OK
+
+        updated_data = update_data_response.json()
+        assert updated_data["first_name"] == update_data["first_name"]
+        assert updated_data["last_name"] == update_data["last_name"]
+
+
+    def test_delete_user(self, app_url, create_user):
+        user_id = create_user
+
+        delete_response = requests.delete(f"{app_url}/api/users/{user_id}")
+        assert delete_response.status_code == HTTPStatus.OK
+        assert delete_response.json() == DELETED_MESSAGE
+
+        get_user_response = requests.get(f"{app_url}/api/users/{user_id}")
+        assert get_user_response.status_code == HTTPStatus.NOT_FOUND
+        assert get_user_response.json() == USER_NOT_FOUND
+
+
+    def test_get_after_create_and_update(self, app_url, create_user_and_delete):
+        user_data = create_user_and_delete
         user_id = user_data["id"]
 
         get_user_by_id = requests.get(f"{app_url}/api/users/{user_id}")
@@ -110,4 +147,24 @@ class TestApi:
         assert update_user_response.status_code == HTTPStatus.OK
         assert updated_data["first_name"] == user_update_data["first_name"]
 
+    def test_method_not_allowed(self, app_url):
+        response = requests.put(f"{app_url}/api/users/1")
+        assert response.status_code == 405
 
+    def test_delete_nonexistent_user(self, app_url):
+        response = requests.delete(f"{app_url}/api/users/99999999")
+        assert response.status_code == HTTPStatus.NOT_FOUND
+
+    def test_update_nonexistent_user(self, app_url):
+        response = requests.patch(f"{app_url}/api/users/99999999", json=dict(first_name="User", last_name="Nonexistent",))
+        assert response.status_code == HTTPStatus.NOT_FOUND
+
+    def test_missing_field(self, app_url):
+        response = requests.post(f"{app_url}/api/users")
+        assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+    def test_invalid_email(self, app_url):
+        data = dict(email="tobias.funke", first_name="Test", last_name="Funke",
+                         avatar="https://reqres.in/img/faces/9-image.jpg")
+        response = requests.post(f"{app_url}/api/users", json=data)
+        assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
