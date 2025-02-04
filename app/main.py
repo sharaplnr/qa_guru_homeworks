774ef2da9621350951cb3bd6007f1744e848ec55
ifsharap <ifsharap@mts.ru>
2025-02-04 17:20:51 +0300

    added code for hw4

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import dotenv
 
 dotenv.load_dotenv()
@@ -5,10 +7,17 @@ dotenv.load_dotenv()
 import uvicorn
 
 from fastapi import FastAPI
-from routers import status, users
-from app.database.engine import create_db_and_tables
 
-app = FastAPI()
+from app.routers import status, users
+from app.database.engine import create_db_and_tables
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("On startup")
+    create_db_and_tables()
+    yield
+    print("On shutdown")
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 
